81478e0eb9f439474d71c4462a5a1c221513cd7d
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-05 21:27:57 +0330

    Created blog app models
    Defined Post and category models

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    """this is a class to define post for blog app"""
+
+    author = models.ForeignKey("accounts.profile", on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey("category", on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def get_snippet(self):
+        return self.content[0:20]
+
+
+class Category(models.Model):
+    """this is a class to define category for blog app"""
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
