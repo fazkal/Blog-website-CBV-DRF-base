4934236fd3882d17b3f768f58b96eeb31ab0021e
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-06 15:37:28 +0330

    Set static and media
    Set static and media root and url for serving in  development state

--- core/core/settings.py
+++ core/core/settings.py
@@ -123,7 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATIC_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
