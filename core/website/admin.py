daabf7d3e5c71cb9c705e530639017e15f63706c
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-06 16:49:07 +0330

    Admin configuration on website admin panel
    For Contact and newsletter

--- core/website/admin.py
+++ core/website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from website.models import contact,newsletter
 
 # Register your models here.
+class websiteAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    list_display=('name','subject','created_date')
+    list_filter=('email',)
+    ordering=['created_date']
+    search_fields=('name','subject','message')
+admin.site.register(contact ,websiteAdmin)
+admin.site.register(newsletter)
