4948670b57ff53b16c32a218ad4167ad71bb455b
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-06 16:42:15 +0330

    create website models
    Defined contact and newsletter models

--- core/website/models.py
+++ core/website/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    subject=models.CharField(max_length=100)
+    message=models.TextField()
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['created_date']
+    def __str__(self):
+        return "{} - {}".format(self.name,self.subject)
+
+class newsletter(models.Model):
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.email
