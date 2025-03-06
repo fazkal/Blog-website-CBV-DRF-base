db7ac5271a9416153f132d6360aa2fbb6d53b3dd
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-06 15:00:59 +0330

    Set admin panel
    Defined a class inherited of UserAdmin for enable display, add and edit on admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "is_superuser", "is_active", "is_verified")
+    searching_fields = ("email",)
+    ordering = ("email",)
+# set fieldset for enable view and edit users
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": ("email", "password"),
+            },
+        ),
+        (
+            "permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                ),
+            },
+        ),
+        (
+            "group permissions",
+            {
+                "fields": ("groups", "user_permissions"),
+            },
+        ),
+        (
+            "important date",
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+# set add_fieldset for enable add users
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(Profile)
+admin.site.register(User, CustomUserAdmin)
