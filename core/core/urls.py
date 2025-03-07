011495049446bfaaf4a24ba06ce0bd46bf28b193
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-07 21:27:08 +0330

    browsable API path
    Defined path for login and logout in browsable API

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # path for login and logout in browsable API 
+    path("api-auth/", include("rest_framework.urls")),
 ]
 # serving static and media for development
 if settings.DEBUG:
