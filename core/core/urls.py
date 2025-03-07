74dc57a4b853928943d62dccbe9f12bcb0f93e02
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-07 21:48:36 +0330

    Added apps paths in core urls
    Including accounts, blog and website.

--- core/core/urls.py
+++ core/core/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("accounts/", include("accounts.urls")),
+    path("blog/", include("blog.urls")),
+    path('',include('website.urls')),
     # path for login and logout in browsable API 
     path("api-auth/", include("rest_framework.urls")),
 ]
