661f641d5f59ef958eed4635ff342bb6b9ca64a8
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-08 14:01:45 +0330

    Define pagination for posts APIs
    Search, pagination and filtering for display APIs data.

--- core/blog/views.py
+++ core/blog/views.py
@@ -2,6 +2,9 @@ from .models import Post, Category
 from .serializers import PostSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .paginations import DefaultPagination
 
 
 # Define PostModelViewSet for get,post,put and delete list or single post
@@ -9,6 +12,12 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["category", "author", "status"]
+    search_fields = ["title", "content"]
+    ordering_fields = ["published_date"]
+    pagination_class = DefaultPagination
+
 
 # Define CategoryModelViewSet for get,post,put and delete list or single category
 class CategoryModelViewSet(viewsets.ModelViewSet):
