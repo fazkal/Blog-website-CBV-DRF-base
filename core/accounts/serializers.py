f18b9b92ec7c3ea8859e6d27e803584047397c94
Fazel KALHORI <fazelkalhory@gmail.com>
2025-03-10 16:53:14 +0330

    Defined serializer for Registration user on accounts app
    For validate password then create user.

---
+++ core/accounts/serializers.py
@@ -0,0 +1,29 @@
+from models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
+from rest_framework import serializers
+
+
+# Define Serializer for registration account
+class RegistrationSerializer(serializers.ModelSerializer):
+    # For confirm password:
+    password1 = serializers.CharField(max_length=225, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "password1"]
+
+    def validate(self, attrs):
+        if attrs.get("password") != attrs.get("password1"):
+            raise serializers.ValidationError({"Detail": "Passwords does not match"})
+
+        try:
+            validate_password(attrs.get("password"))
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        validated_data.pop("password1", None)
+        return User.objects.create_user(**validated_data)
