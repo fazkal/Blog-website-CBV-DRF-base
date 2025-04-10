2dff5e9f268bc233e5ada409c4ea80aa5366d0ec
Fazel KALHORI <fazelkalhory@gmail.com>
2025-04-10 17:33:43 +0330

    Add jquery static files

--- core/staticfiles/js/main.js
+++ core/staticfiles/js/main.js
@@ -1,398 +1,135 @@
-$(document).ready(function() {
-    "use strict";
+// Some CSS depends on the `js` class.
+$( "html" ).addClass( "js" ).removeClass( "no-js" );
 
+/*
+ * All sites
+ */
+$(function() {
+	// copyable auto-select-all
+	$( ".copyable" ).on( "click", function() {
+		if ( typeof this.select === "function" ) {
+			this.select();
+		}
+	});
 
-    var window_width = $(window).width(),
-        window_height = window.innerHeight,
-        header_height = $(".default-header").height(),
-        header_height_static = $(".site-header.static").outerHeight(),
-        fitscreen = window_height - header_height;
-
-    $(".fullscreen").css("height", window_height)
-    $(".fitscreen").css("height", fitscreen);
-
-
-    // ------- Datepicker  js --------//  
-
-      $( function() {
-        $( ".date-picker" ).datepicker();
-      } );
-
-
-    //------- Niceselect  js --------//  
-
-    if (document.getElementById("default-select")) {
-        $('select').niceSelect();
-    };
-    if (document.getElementById("default-select2")) {
-        $('select').niceSelect();
-    };
-    if (document.getElementById("service-select")) {
-        $('select').niceSelect();
-    };    
-
-    //------- Lightbox  js --------//  
-
-    $('.img-gal').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
-
-    $('.play-btn').magnificPopup({
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
-
-    //------- Superfish nav menu  js --------//  
-
-    $('.nav-menu').superfish({
-        animation: {
-            opacity: 'show'
-        },
-        speed: 400
-    });
-
-
-
-    //------- Owl Carusel  js --------//  
-
-    $('.active-hot-deal-carusel').owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        autoplayHoverPause: true,        
-        smartSpeed:500,          
-        margin:30,
-        dots: true
-    });
-
-     $('.active-testimonial').owlCarousel({
-            items: 2,
-            loop: true,
-            margin: 30,
-            autoplayHoverPause: true,
-            smartSpeed:500,              
-            dots: true,
-            autoplay: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                480: {
-                    items: 1,
-                },
-                992: {
-                    items: 2,
-                }
-            }
-        });
-
-
-        $('.active-recent-blog-carusel').owlCarousel({
-            items: 3,
-            loop: true,
-            margin: 30,
-            dots: true,
-            autoplayHoverPause: true, 
-            smartSpeed:500,               
-            autoplay: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                480: {
-                    items: 1,
-                },
-                768: {
-                    items: 2,
-                },
-                961: {
-                    items: 3,
-                }
-            }
-        }); 
-
-    //------- Mobile Nav  js --------//  
-
-    if ($('#nav-menu-container').length) {
-        var $mobile_nav = $('#nav-menu-container').clone().prop({
-            id: 'mobile-nav'
-        });
-        $mobile_nav.find('> ul').attr({
-            'class': '',
-            'id': ''
-        });
-        $('body .main-menu').append($mobile_nav);
-        $('body .main-menu').prepend('<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
-        $('body .main-menu').append('<div id="mobile-body-overly"></div>');
-        $('#mobile-nav').find('.menu-has-children').prepend('<i class="lnr lnr-chevron-down"></i>');
-
-        $(document).on('click', '.menu-has-children i', function(e) {
-            $(this).next().toggleClass('menu-item-active');
-            $(this).nextAll('ul').eq(0).slideToggle();
-            $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
-        });
-
-        $(document).on('click', '#mobile-nav-toggle', function(e) {
-            $('body').toggleClass('mobile-nav-active');
-            $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-            $('#mobile-body-overly').toggle();
-        });
-
-            $(document).on('click', function(e) {
-            var container = $("#mobile-nav, #mobile-nav-toggle");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-                    $('#mobile-body-overly').fadeOut();
-                }
-            }
-        });
-    } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-        $("#mobile-nav, #mobile-nav-toggle").hide();
-    }
-
-    //------- Smooth Scroll  js --------//  
-
-    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            if (target.length) {
-                var top_space = 0;
-
-                if ($('#header').length) {
-                    top_space = $('#header').outerHeight();
-
-                    if (!$('#header').hasClass('header-fixed')) {
-                        top_space = top_space;
-                    }
-                }
-
-                $('html, body').animate({
-                    scrollTop: target.offset().top - top_space
-                }, 1500, 'easeInOutExpo');
-
-                if ($(this).parents('.nav-menu').length) {
-                    $('.nav-menu .menu-active').removeClass('menu-active');
-                    $(this).closest('li').addClass('menu-active');
-                }
-
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('#mobile-nav-toggle i').toggleClass('lnr-times lnr-bars');
-                    $('#mobile-body-overly').fadeOut();
-                }
-                return false;
-            }
-        }
-    });
-
-    $(document).ready(function() {
-
-        $('html, body').hide();
-
-        if (window.location.hash) {
-
-            setTimeout(function() {
-
-                $('html, body').scrollTop(0).show();
-
-                $('html, body').animate({
-
-                    scrollTop: $(window.location.hash).offset().top - 108
-
-                }, 1000)
-
-            }, 0);
-
-        } else {
-
-            $('html, body').show();
-
-        }
-
-    });
-
-
-    jQuery(document).ready(function($) {
-        // Get current path and find target link
-        var path = window.location.pathname.split("/").pop();
-
-        // Account for home page with empty path
-        if (path == '') {
-            path = 'index.html';
-        }
-
-        var target = $('nav a[href="' + path + '"]');
-        // Add active class to target link
-        target.addClass('menu-active');
-    });
-
-    $(document).ready(function() {
-        if ($('.menu-has-children ul>li a').hasClass('menu-active')) {
-            $('.menu-active').closest("ul").parentsUntil("a").addClass('parent-active');
-        }
-    });
+	// collapsible navigation for smaller screens
+	$('.menu-trigger').on('click', function (e) {
+		var $btn = $(this),
+			$nav = $(this).next('.menu');
+		if ($nav.hasClass('menu-open')) {
+			$btn.attr('aria-expanded', 'false');
+			$nav.removeClass('menu-open');
+		} else {
+			$btn.attr('aria-expanded', 'true');
+			$nav.addClass('menu-open');
+			$nav.find('a').first().focus();
+		}
+	});
+});
 
 
 
 
-    //------- Header Scroll Class  js --------//  
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('#header').addClass('header-scrolled');
-        } else {
-            $('#header').removeClass('header-scrolled');
-        }
-    });
+/*
+ * API sites
+ */
+$(function() {
+	$( ".entry-example" ).each(function() {
+		var iframeSrc,
+			src = $( this ).find( ".syntaxhighlighter" ),
+			output = $( this ).find( ".code-demo" );
 
-    //------- Google Map  js --------//  
+		if ( !src.length || !output.length ) {
+			return;
+		}
 
-    if (document.getElementById("map")) {
-        google.maps.event.addDomListener(window, 'load', init);
+		// Get the original source
+		iframeSrc = src.find( "td.code .line" ).map(function() {
+			// Convert non-breaking spaces from highlighted code to normal spaces
+			return $( this ).text().replace( /\xa0/g, " " );
+		// Restore new lines from original source
+		}).get().join( "\n" );
 
-        function init() {
-            var mapOptions = {
-                zoom: 11,
-                center: new google.maps.LatLng(40.6700, -73.9400), // New York
-                styles: [{
-                    "featureType": "water",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#e9e9e9"
-                    }, {
-                        "lightness": 17
-                    }]
-                }, {
-                    "featureType": "landscape",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#f5f5f5"
-                    }, {
-                        "lightness": 20
-                    }]
-                }, {
-                    "featureType": "road.highway",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                        "color": "#ffffff"
-                    }, {
-                        "lightness": 17
-                    }]
-                }, {
-                    "featureType": "road.highway",
-                    "elementType": "geometry.stroke",
-                    "stylers": [{
-                        "color": "#ffffff"
-                    }, {
-                        "lightness": 29
-                    }, {
-                        "weight": 0.2
-                    }]
-                }, {
-                    "featureType": "road.arterial",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#ffffff"
-                    }, {
-                        "lightness": 18
-                    }]
-                }, {
-                    "featureType": "road.local",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#ffffff"
-                    }, {
-                        "lightness": 16
-                    }]
-                }, {
-                    "featureType": "poi",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#f5f5f5"
-                    }, {
-                        "lightness": 21
-                    }]
-                }, {
-                    "featureType": "poi.park",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#dedede"
-                    }, {
-                        "lightness": 21
-                    }]
-                }, {
-                    "elementType": "labels.text.stroke",
-                    "stylers": [{
-                        "visibility": "on"
-                    }, {
-                        "color": "#ffffff"
-                    }, {
-                        "lightness": 16
-                    }]
-                }, {
-                    "elementType": "labels.text.fill",
-                    "stylers": [{
-                        "saturation": 36
-                    }, {
-                        "color": "#333333"
-                    }, {
-                        "lightness": 40
-                    }]
-                }, {
-                    "elementType": "labels.icon",
-                    "stylers": [{
-                        "visibility": "off"
-                    }]
-                }, {
-                    "featureType": "transit",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#f2f2f2"
-                    }, {
-                        "lightness": 19
-                    }]
-                }, {
-                    "featureType": "administrative",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                        "color": "#fefefe"
-                    }, {
-                        "lightness": 20
-                    }]
-                }, {
-                    "featureType": "administrative",
-                    "elementType": "geometry.stroke",
-                    "stylers": [{
-                        "color": "#fefefe"
-                    }, {
-                        "lightness": 17
-                    }, {
-                        "weight": 1.2
-                    }]
-                }]
-            };
-            var mapElement = document.getElementById('map');
-            var map = new google.maps.Map(mapElement, mapOptions);
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(40.6700, -73.9400),
-                map: map,
-                title: 'Snazzy!'
-            });
-        }
-    }
+		iframeSrc = iframeSrc
+			// Insert styling for the live demo that we don't want in the
+			// highlighted code
+			.replace( "</head>",
+				"<style>" +
+					"html, body { border:0; margin:0; padding:0; }" +
+					"body { font-family: 'Helvetica', 'Arial',  'Verdana', 'sans-serif'; }" +
+				"</style>" +
+				"</head>" )
+			// IE <10 executes scripts in the order in which they're loaded,
+			// not the order in which they're written. So we need to defer inline
+			// scripts so that scripts which need to be fetched are executed first.
+			.replace( /<script>([\s\S]+)<\/script>/,
+				"<script>" +
+				"window.onload = function() {" +
+					"$1" +
+				"};" +
+				"</script>" );
 
-    //------- Mailchimp js --------//  
+		var iframe = document.createElement( "iframe" );
+		iframe.width = "100%";
+		iframe.height = output.attr( "data-height" ) || 250;
+		output.append( iframe );
 
-    $(document).ready(function() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    });
+		var doc = (iframe.contentWindow || iframe.contentDocument).document;
+		doc.write( iframeSrc );
+		doc.close();
+	});
+});
 
+/*
+ * jqueryui.com
+ */
+$(function() {
+	var demoFrame = $( ".demo-frame" ),
+		demoDescription = $( ".demo-description" ),
+		sourceView = $( ".view-source > div" ),
+		demoList = $( ".demo-list" ),
+		currentDemo = location.hash.substring( 1 );
+
+	demoList.on( "click", "a", function( event ) {
+		event.preventDefault();
+
+		var filename = "/" + event.target.pathname.replace( /^\//, "" ),
+			parts = filename.split( "/" ),
+			plugin = parts[ 3 ],
+			demo = parts[ 4 ].substring( 0, parts[ 4 ].length - 5 );
+
+		if ( demoList.is( "[data-full-nav]" ) ) {
+			window.location = "/" + (demo === "default" ? plugin : demo) + "/";
+			return;
+		}
+
+		$.getJSON( "/resources/demos/demo-list.json" ).then(function( demoList ) {
+			demoDescription.html( $.grep( demoList[ plugin ], function( x ) {
+				return x.filename === demo;
+			})[ 0 ].description );
+			demoFrame.attr( "src", filename );
+		});
+
+		$.get( filename.replace( "demos", "demos-highlight" ) ).then(function( content ) {
+			sourceView.html( content );
+		});
+
+		demoList.find( ".active" ).removeClass( "active" );
+		$( this ).parent().addClass( "active" );
+		location.hash = "#" + demo;
+	});
+
+	$( ".view-source a" ).on( "click", function() {
+		sourceView.animate({
+			opacity: "toggle",
+			height: "toggle"
+		});
+	});
+
+	if ( currentDemo ) {
+		demoList.find( "a" ).filter(function() {
+			return this.pathname.split( "/" )[ 4 ] === (currentDemo + ".html");
+		}).click();
+	}
 });
