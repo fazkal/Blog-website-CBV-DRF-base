5a223eaf42e46cb6fb568076d0da6e8cec078849
Fazel KALHORI <fazelkalhory@gmail.com>
2025-04-14 18:10:09 +0330

    Changed inheritance of BlogsView to ListView

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,11 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from .models import Post
 
 # TemplateView for display posts
-class BlogsView(TemplateView):
+class BlogsView(ListView):
+    model = Post
+    paginate_by = 2
     template_name = "blog/blog-home.html"
 
 
