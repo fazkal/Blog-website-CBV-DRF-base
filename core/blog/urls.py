794f4434a44ee46f91de2ef6dd711c423f7554cb
Fazel KALHORI <fazelkalhory@gmail.com>
2025-04-17 21:17:41 +0330

    Defined view and url for create post

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 
     path("api/v1/", include("blog.api.v1.urls")),
     path("",views.BlogsView.as_view(),name="index"),
-    path("<int:pk>/",views.BlogSingleView.as_view(),name="single")
+    path("<int:pk>/",views.BlogSingleView.as_view(),name="single"),
+    path("create/",views.PostCreateView.as_view(),name="create"),
+    path("post-form/", views.PostFormView.as_view(), name="post_form")
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic.list import ListView
+from django.views.generic import ListView,CreateView
+from django.views import View
 from .models import Post
+from accounts.models import Profile
+from .forms import PostForm
 
 # TemplateView for display posts
 class BlogsView(ListView):
@@ -12,3 +16,27 @@ class BlogsView(ListView):
 # TemplateView for display a single post
 class BlogSingleView(TemplateView):
     template_name = "blog/blog-single.html"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    # success_url = "/blog/"
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        user_profile = getattr(self.request.user, 'profile', None)
+        if user_profile:
+            form.instance.author = user_profile
+        else:
+            # Handle case when profile is missing, e.g., create one or return error
+            # You can raise an exception or return an error message
+            form.add_error(None, 'User profile does not exist.')
+            return self.form_invalid(form)
+        return super().form_valid(form)
+    
+
+class PostFormView(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {'form': form})
