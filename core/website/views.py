97386d9bc751a2c4bd41d73ac614044a6d0c7e8f
Fazel KALHORI <fazelkalhory@gmail.com>
2025-04-19 17:31:38 +0330

    Defined url and view for NewsletterView
    Also edited ContactView

--- core/website/views.py
+++ core/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView,FormView
+from .forms import ContactForm,NewsletterForm
 # Create your views here.
 
 class IndexView (TemplateView):
@@ -8,5 +9,20 @@ class IndexView (TemplateView):
 class AboutView(TemplateView):
     template_name = "website/about.html"
 
-class ContactView(TemplateView):
+class ContactView(FormView):
     template_name = "website/contact.html"
+    form_class = ContactForm
+    success_url = "/contact"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+class NewsletterView(FormView):
+    template_name = "website/base.html"
+    form_class = NewsletterForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
