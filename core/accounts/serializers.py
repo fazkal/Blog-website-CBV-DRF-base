b8b855fb4d9841c85349870dc52385feb82a9ba8
Fazel KALHORI <fazelkalhory@gmail.com>
2025-04-30 13:05:30 +0330

    Defined serializer for CustomTokenObtainPairView

--- core/accounts/serializers.py
+++ core/accounts/serializers.py
@@ -1,6 +1,8 @@
 from .models import User,Profile
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 
@@ -79,3 +81,32 @@ class ProfileSerializer(serializers.ModelSerializer):
             "image",
             "description",
         )
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['email'] = user.email
+        return token
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if not email or not password:
+            raise serializers.ValidationError("Must include 'email' and 'password'.")
+
+        user = authenticate(request=self.context.get('request'), email=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid login credentials.")
+
+        if not user.is_active:
+            raise serializers.ValidationError("User account is disabled.")
+
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data['email'] = self.user.email
+        return data
